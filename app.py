4ef798a4b3c728d8decf873af191303325e51137
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-12-15 10:44:04 -0500

    yo the fukcin abt page holy sfhit

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "welcome to our Flask Web Application!"
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
