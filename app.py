263e79ea2fc61407fc0ac2b9076e4c91f46383c3
StealerOfKneecaps <zhao.lucas@outlook.com>
2026-02-25 21:12:40 -0500

    me when I actually wokr on this no way

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect, url_for
 import sqlite3
 app = Flask(__name__)
 
@@ -46,18 +46,36 @@ def init_db():
 init_db()
 @app.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", methods=["GET","POST"])
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    error = None
+    if request.method == "POST":
+        conn = sqlite3.connect("database.db")
+        c = conn.cursor()
+        email = request.form["email"]
+        password = request.form["password"]
+        try:
+            c.execute("INSERT INTO users (email, passwordHash, role) VALUES (?, ?, ?)", (email, password, 1))
+            conn.commit()
+            conn.close()
+            return redirect(url_for("login"))
+        except sqlite3.IntegrityError:
+            error = "bro hop off"
+            conn.close()
+    return render_template("register.html", error=error)
 
 @app.route("/about")
 def about():
     return render_template("about.html")
     
-@app.route("/mainThing")
+@app.route("/mainThing", methods=["GET","POST"])
 def mainThing():
     if request.method == "POST":
-    return render_template("mainThing.html")
+        return render_template("mainThing.html")
 
-@app.route("/resultsPage", method=["GET", "POST"])
+@app.route("/resultsPage", methods=["GET", "POST"])
 def resultsPage():
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
@@ -65,6 +83,7 @@ def resultsPage():
         name = request.form["name"]
         c.execute("INSERT INTO userInfoCache (name, ) VALUES (?,)", (name, ))
     return render_template("resultsPage.html")
+
 
 
 if __name__ == "__main__":
